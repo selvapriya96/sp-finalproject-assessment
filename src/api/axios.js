f68a17067c6c33e4ff307bf1a5d13ceecc642e74
selvapriya96 <rsselvapriya96@gmail.com>
2025-11-24 18:25:55 +0530

    corrected route and bd url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api", // your backend
+  baseURL: "https://sp-finalproject-assessment-bd.onrender.com", // your backend
 });
 
 export default API;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -15,7 +15,7 @@ export default function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await API.post("/auth/register", form);
+      const res = await API.post("/api/auth/register", form);
       setMessage(res.data.message || "Registered successfully!");
 
       alert("🎉 Registration successful! Please login now.");
